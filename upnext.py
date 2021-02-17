6dfc9283e5f43c126a3b180ba8b7e106b8499e2c
hbkpancakes <74506492+hbkpancakes@users.noreply.github.com>
2021-02-16 19:02:15 -0500

    Update upnext.py

--- upnext.py
+++ upnext.py
@@ -1,7 +1,20 @@
-# Upnext BETA by hbk Pancakes
-# Upnext is a YouTube playlist builder, which will remove old
-# videos and add new videos to a specified playlist, based on 
-# channel subscription choices that you specify.
+# Upnext (BETA) by hbk Pancakes
+# Upnext is a YouTube playlist builder, which will remove old videos and add new videos to a specified playlist, based on channel subscription choices that you specify.
+
+"""
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+"""
 
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
